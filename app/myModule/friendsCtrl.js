14ae1c7c0d58b6bf0343323cb2fc901622ff750d
Flur <superflur@gmail.com>
2015-02-08 14:36:42 +0200

    its almost working

--- app/myModule/friendsCtrl.js
+++ app/myModule/friendsCtrl.js
@@ -3,12 +3,21 @@ angular.module('myModule').controller('friendsCtrl', ['$scope', '$http', '$state
 
 function friendsCtrl($scope, $http, $stateParams, _pageTitle, _appUser) {
 
-    $scope.params = $stateParams;
-
 
     $http.get('http://api.ember-cli-101.com/api/friends').success(function(data) {
-    	$scope.some = data;
+    	$scope.friends = data.friends;
     });
 }
 
-
+// angular.module('myModule').factory('myFactory', [function() {
+// 	return {
+// 		parse: function(object) {
+// 			var parsedArray = [];
+// 			for (var i = 0 ; i < object.length ; i++) {
+// 				parsedArray[i] = angular.fromJson(object[i]);
+// 				console.log(parsedArray[i]);
+// 			}
+// 			return parsedArray;
+// 		}
+// 	}
+// }])
